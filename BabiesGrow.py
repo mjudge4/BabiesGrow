ae884c7edf7d27c3a813c11eb593a6297d386120
mjudge4 <mjudge4@gmail.com>
2018-02-19 20:01:34 +0000

    FB login seems to be working...somehow

--- BabiesGrow.py
+++ BabiesGrow.py
@@ -41,42 +41,53 @@ def fbconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     access_token = request.data
+    print "access token received %s " % access_token
 
 
-    #Exchange client token for long lived server-side token
-    app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
-    app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id,app_secret, access_token)
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
+        'web']['app_id']
+    app_secret = json.loads(
+        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
-    #Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v2.12/me"
-    #strip expire tag from access token
-    token = result.split(",")[0].split(':')[1].replace('"','')
+
+    # Use token to get user info from API
+    userinfo_url = "https://graph.facebook.com/v2.8/me"
+    '''
+        Due to the formatting for the result from the server token exchange we have to
+        split the token first on commas and select the first index which gives us the key : value
+        for the server access token then we split it on colons to pull out the actual token value
+        and replace the remaining quotes with nothing so that it can be used directly in the graph
+        api calls
+    '''
+    token = result.split(',')[0].split(':')[1].replace('"', '')
 
     url = 'https://graph.facebook.com/v2.12/me?access_token=%s&fields=name,id,email' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-    #print "url sent for API access:%s"% url
-    #print "API JSON result: %s" % result
+    # print "url sent for API access:%s"% url
+    # print "API JSON result: %s" % result
     data = json.loads(result)
     login_session['provider'] = 'facebook'
     login_session['username'] = data["name"]
     login_session['email'] = data["email"]
     login_session['facebook_id'] = data["id"]
 
+    # The token must be stored in the login_session in order to properly logout
     login_session['access_token'] = token
 
-    #Get the user's picture
-    url = 'https://graph.facebook.com/v2.12/me/picture?%s&redirect=0&height=200&width=200' % token
+    # Get user picture
+    url = 'https://graph.facebook.com/v2.12/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
 
     login_session['picture'] = data["data"]["url"]
 
-    # Check if user exists
+    # see if user exists
     user_id = getUserID(login_session['email'])
     if not user_id:
         user_id = createUser(login_session)
@@ -85,12 +96,13 @@ def fbconnect():
     output = ''
     output += '<h1>Welcome, '
     output += login_session['username']
+
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
     output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
-    flash("you are now logged in as %s" % login_session['username'])
-    print "done!"
+
+    flash("Now logged in as %s" % login_session['username'])
     return output
 
 @app.route('/fbdisconnect', methods=['POST'])
@@ -109,7 +121,7 @@ def fbdisconnect():
 
 # Disconnect based on provider
 
-@app.route('disconnect')
+@app.route('/disconnect')
 def disconnect():
     if 'provider' in login_session:
         if login_session['provider'] == 'google':
